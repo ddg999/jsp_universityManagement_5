23c682e14a44bb7db6d2ebb106a41e1cf549fc3d
ddg999 <slowman918@gmail.com>
2024-07-17 14:47:56 +0900

    first commit

---
+++ src/main/java/com/tenco/university/util/DBUtil.java
@@ -0,0 +1,25 @@
+package com.tenco.university.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBUtil {
+	private static DataSource dataSource;
+
+	static {
+		try {
+			InitialContext ctx = new InitialContext();
+			dataSource = (DataSource) ctx.lookup("java:comp/env/jdbc/university");
+		} catch (Exception e) {
+			System.out.println("DBUtil 초기화 실패");
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return dataSource.getConnection();
+	}
+}
