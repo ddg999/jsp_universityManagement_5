fc4257b3345af574a9c84a2f4e931bbf0fe9bc4b
HyeonProG <guswo3443@naver.com>
2024-07-18 14:39:01 +0900

    학생 DTO

--- src/main/java/com/university/model/StuSub.java
+++ src/main/java/com/university/model/StuSub.java
@@ -1,5 +1,22 @@
 package com.university.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class StuSub {
 
+	private int id;
+	private int studentId;
+	private int subjectId;
+	private String grade;
+	private int completeGrade;
+	
 }
--- src/main/java/com/university/model/StuSubDetail.java
+++ src/main/java/com/university/model/StuSubDetail.java
@@ -2,4 +2,14 @@ package com.university.model;
 
 public class StuSubDetail {
 
+	private int id;
+	private int studentId;
+	private int subjectId;
+	private int absent;
+	private int lateness;
+	private int homework;
+	private int midExam;
+	private int finalExam;
+	private int convertedMark;
+	
 }
