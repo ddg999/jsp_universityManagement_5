907593b67d3e80f03546c033dc7156b55a626662
nambanote <namcher9428@gmail.com>
2024-07-19 17:56:27 +0900

    주윤재

--- src/main/java/com/university/repository/interfaces/ProfessorRepository.java
+++ src/main/java/com/university/repository/interfaces/ProfessorRepository.java
@@ -13,18 +13,11 @@ import com.university.model.Syllabus;
 
 public interface ProfessorRepository {
 
-	//TODO 내 강의 학기별 조회
-	Subject getProfessorSubjectbyYear(int professorID, int subYear);
-	Subject getProfessorSubjectbyYearandSemester(int professorID, int subYear,int semester);
+
 	// TODO 강의 계획서 수정
 	Syllabus getsyllabus(Subject subject); // 임시코드
 	// TODO 강의별 학생리스트, 조회, 출결 및 성적기입
 	
-	// 강의별 학생리스트 전체 조회
-	List<StuSubDetail> getAllStuSubDetails();
 	
-	StuSubDetail getStuSubDetail(Student student, Subject subject); // 임시코드
-	// TODO 강의평가 확인
-	Evaluation getEvaluation(Subject subject); //임시코드
 	
 }
