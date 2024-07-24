e91fd82287650ba93cc9c44b123ca178567dc0ee
hyeongjeongyoo <hjeong0711@gmail.com>
2024-07-24 17:20:49 +0900

    학생명단조회 페이징 처리 완료

--- src/main/java/com/university/repository/StudentRepositoryImpl.java
+++ src/main/java/com/university/repository/StudentRepositoryImpl.java
@@ -11,11 +11,12 @@ import com.university.repository.interfaces.StudentRepository;
 import com.university.util.DBUtil;
 
 public class StudentRepositoryImpl implements StudentRepository {
-	
-	private static final String SEARCH_STUDENT_ID = " SELECT * FROM student_tb WHERE id LIKE ? OR name ORDER BY id DESC LIMIT ? OFFSET ? ";
+
 	private static final String SEARCH_STUDENT_ID_AND_DEPT_ID = " SELECT * FROM student_tb WHERE id LIKE ? OR dept_id LIKE ? ORDER BY id DESC LIMIT ? OFFSET ? ";
-	private static final String SEARCH_STUDNET_NAME = " select * from student_tb where name like ? ";
-	
+	private static final String SEARCH_STUDNET_NAME = " SELECT * FROM student_tb WHERE name LIKE ? ORDER BY name DESC LIMIT ? OFFSET ? ";
+	private static final String SELECT_STUDENT_BY_NAME = " SELECT * FROM student_tb WHERE name LIKE ? ORDER BY name DESC LIMIT ? OFFSET ? ";
+	private static final String COUNT_SEARCH_STUDENT = " SELECT COUNT(*) as count FROM student_tb WHERE name LIKE ? ";
+	private static final String COUNT_STUDEN_BY_NAME = " SELECT count(*) count from student_tb WHERE name LIKE ? ";
 	
 	
 	@Override
@@ -24,24 +25,15 @@ public class StudentRepositoryImpl implements StudentRepository {
 		try (Connection conn = DBUtil.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(SEARCH_STUDNET_NAME)) {
 			pstmt.setString(1, "%" + keyword + "%");
-//			pstmt.setInt(2, pageSize);
-//			pstmt.setInt(3, offset);
+			pstmt.setInt(2, pageSize);
+			pstmt.setInt(3, offset);
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
-				System.out.println("11");
-				studentList.add(Student.builder()
-						.id(rs.getInt("id"))
-						.name(rs.getString("name"))
-						.birthDate(rs.getDate("birth_date"))
-						.gender(rs.getString("gender"))
-						.address(rs.getString("address"))
-						.tel(rs.getString("tel"))
-						.email(rs.getString("email"))
-						.deptId(rs.getInt("dept_id"))
-						.grade(rs.getInt("grade"))
-						.semester(rs.getInt("semester"))
-						.entranceDate(rs.getDate("entrance_date"))
-						.graduationDate(rs.getDate("graduation_date"))
+				studentList.add(Student.builder().id(rs.getInt("id")).name(rs.getString("name"))
+						.birthDate(rs.getDate("birth_date")).gender(rs.getString("gender"))
+						.address(rs.getString("address")).tel(rs.getString("tel")).email(rs.getString("email"))
+						.deptId(rs.getInt("dept_id")).grade(rs.getInt("grade")).semester(rs.getInt("semester"))
+						.entranceDate(rs.getDate("entrance_date")).graduationDate(rs.getDate("graduation_date"))
 						.build());
 			}
 
@@ -51,11 +43,30 @@ public class StudentRepositoryImpl implements StudentRepository {
 		return studentList;
 	}
 
+
+	@Override
+	public int getTotalStudentCountByTitle(String keyword, int pageSize, int offset) {
+		int count = 0;
+		try (Connection conn = DBUtil.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(SELECT_STUDENT_BY_NAME)) {
+			pstmt.setString(1, "%" + keyword + "%");
+			pstmt.setInt(2, pageSize);
+			pstmt.setInt(3, offset);
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				count = rs.getInt("count");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return count;
+	}
+	
+
 	@Override
 	public List<Student> getStudentDeptId(String keyword, int pageSize, int offset) {
 		return null;
 	}
-
 
 	@Override
 	public List<Student> getStudentIdDeptId(String keyword, int pageSize, int offset) {
@@ -81,15 +92,29 @@ public class StudentRepositoryImpl implements StudentRepository {
 	}
 
 	@Override
-	public int getTotalStudentCountByTitle(String keyword) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
 	public int getTotalStudentCountByTitleOrContent(String keyword) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	@Override
+	public int getTotalStudentNameCount(String studentName) {
+		int count = 0;
+		
+		try (Connection conn = DBUtil.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(COUNT_SEARCH_STUDENT)) {
+			pstmt.setString(1, "%" + studentName + "%");
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				count = rs.getInt("count");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("searchStudent totalCount : " + count);
+		return count;
+	}
+
+	
+	
 }
